f123ccd4426c3241e287a784de89866f8b1d6764
Rama <hermi.ramaa@gmail.com>
2024-03-29 09:33:57 +0100

    Ajoutez des fichiers projet.

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    public interface IGenericRepository <T> where T : class
+    {
+        Task<List<T>> CreateAsync (T entity);
+        Task DeleteAsync (T entity);
+        Task<T> UpdateAsync (T entity);
+        
+        Task<T> GetAsync();
+        Task<T> GetByIdAsync (int id);  
+
+    }
+
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        //injection we need repository
+        public CreateLeaveTypeCommandHandler(IMapper mapper , ILeaveTypeRepository LeaveTypeRepository)
+        {
+            this._mapper = mapper;
+            _leaveTypeRepository = LeaveTypeRepository;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            //validate incoming data
+            //convert to domain entity obj
+            var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+            //add to db 
+
+            await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+            // return record
+            return leaveTypeToCreate.Id;
+        }
+    }
+}
